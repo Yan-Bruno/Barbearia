e7b1fc7ed52bc130b042f3576037a587ef4e0b61
Yan-Bruno <valebrunoyan@gmail.com>
2025-11-10 22:26:13 -0300

    chore:add prisma setup

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url:
+      process.env.DATABASE_URL ||
+      "postgresql://user:password@localhost:5432/db",
+  },
+});
